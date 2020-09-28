c81b5ee88b25a9233c4ded14b07e00443f80b91e
Viktor Merkurev <6966509+fennr@users.noreply.github.com>
2020-09-28 21:56:05 +0500

    telebot test

--- src/bot.py
+++ src/bot.py
@@ -7,6 +7,7 @@ This program is dedicated to the public domain under the CC0 license.
 """
 import logging
 import telegram
+import telebot
 from telegram.error import NetworkError, Unauthorized
 from time import sleep
 
@@ -15,42 +16,17 @@ update_id = None
 
 BOT_TOKEN = '1200994135:AAGwosYLPwpVDfLahuBIjNduhsMq2WMrp1Q'
 
-def main():
-    """Run the bot."""
-    global update_id
-    # Telegram Bot Authorization Token
-    bot = telegram.Bot(BOT_TOKEN)
+bot = telebot.TeleBot(BOT_TOKEN)
 
-    # get the first pending update_id, this is so we can skip over it in case
-    # we get an "Unauthorized" exception.
-    try:
-        update_id = bot.get_updates()[0].update_id
-    except IndexError:
-        update_id = None
+@bot.message_handler(commands=['start'])
+def start_message(message):
+bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+if message.text.lower() == 'привет':
+bot.send_message(message.chat.id, 'Привет, мой создатель')
+elif message.text.lower() == 'пока':
+bot.send_message(message.chat.id, 'Прощай, создатель')
 
-    while True:
-        try:
-            echo(bot)
-        except NetworkError:
-            sleep(1)
-        except Unauthorized:
-            # The user has removed or blocked the bot.
-            update_id += 1
-
-
-def echo(bot):
-    """Echo the message the user sent."""
-    global update_id
-    # Request updates after the last update_id
-    for update in bot.get_updates(offset=update_id, timeout=10):
-        update_id = update.update_id + 1
-
-        if update.message:  # your bot can receive updates without messages
-            # Reply to the message
-            update.message.reply_text(update.message.text)
-
-
-if __name__ == '__main__':
-    main()
+bot.polling())
