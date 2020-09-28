3b38c068c1421fbf549ac555dc2c0ac50d8923f6
Viktor Merkurev <6966509+fennr@users.noreply.github.com>
2020-09-28 21:59:40 +0500

    fix

--- src/bot.py
+++ src/bot.py
@@ -20,13 +20,13 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-if message.text.lower() == 'привет':
-bot.send_message(message.chat.id, 'Привет, мой создатель')
-elif message.text.lower() == 'пока':
-bot.send_message(message.chat.id, 'Прощай, создатель')
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
 
-bot.polling())
+bot.polling()
